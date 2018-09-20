4077287b10a0dd5570c37cc3bd4b74a3466e56ba
Fred Z <freezed@users.noreply.github.com>
2018-09-20 13:55:25 +0200

    Add django app, ersatz #1

---
+++ ersatz/urls.py
@@ -0,0 +1,10 @@
+"""
+ersatz app URL Configuration
+"""
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+]
--- omega/urls.py
+++ omega/urls.py
@@ -2,11 +2,13 @@
 omega URL Configuration
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from . import views
+import ersatz
 
 urlpatterns = [
     path('', views.index),
+    path('ersatz/', include('ersatz.urls')),
     path('admin/', admin.site.urls),
 ]
