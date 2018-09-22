1406fa97536e2ad2dfbf2852131a6b8f1a664ca8
Fred Z <freezed@users.noreply.github.com>
2018-09-23 00:40:36 +0200

    Add a user login template #6

--- omega/urls.py
+++ omega/urls.py
@@ -8,6 +8,7 @@ from omega import views as omega_views
 
 urlpatterns = [
     path('', omega_views.index, name='home'),
+    path('my/', include('django.contrib.auth.urls')),
     path('account/', include('account.urls')),
     path('ersatz/', include('ersatz.urls')),
     path('admin/', admin.site.urls),
