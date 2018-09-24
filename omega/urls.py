7b9dbb842fe7d26c05cad2e3c8b6f2f5a86d80ed
Fred Z <freezed@users.noreply.github.com>
2018-09-24 16:26:23 +0200

    Add signin feature #6

--- omega/urls.py
+++ omega/urls.py
@@ -8,6 +8,7 @@ from omega import views as omega_views
 
 urlpatterns = [
     path('', omega_views.index, name='home'),
+    path('my/signin/', omega_views.signin, name='signin'),
     path('my/', include('django.contrib.auth.urls')),
     path('account/', include('account.urls')),
     path('ersatz/', include('ersatz.urls')),
--- omega/views.py
+++ omega/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 def index(request):
     return render(request, 'omega/home.html', {'context':'project index'})
+
+def signin(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signin.html', {'form': form})
