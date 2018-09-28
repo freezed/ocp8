e99ba7163bbb687fe3f73717079bcc14e09673b6
Fred Z <freezed@users.noreply.github.com>
2018-09-28 15:59:58 +0200

    Add tests for ersatz.views

--- ersatz/tests.py
+++ ersatz/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+import pytest
+from . import views
 
-# Create your tests here.
+###########
+## VIEWS ##
+###########
+class InvalidRequest:
+    META = {'QUERY_STRING': '?foo=bar&q=foobar'}
+
+def test_invalid_request():
+    request = InvalidRequest()
+    response = views.search(request)
+    assert b'Status : False' in response.content
+
+class ValidRequest:
+    META = {'QUERY_STRING': '?foo=bar&s=sel'}
+    GET = {'s': 'sel'}
+
+def test_valid_request():
+    request = ValidRequest()
+    response = views.search(request)
+    assert b'Status : True' in response.content
