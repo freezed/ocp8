8cc730eda892624f1fd8455e2c9f97921624939b
Fred Z <freezed@users.noreply.github.com>
2018-10-03 14:57:50 +0200

    Correct wrong '_payload.update()' FIX #18
    Add doc about response formatting and conform `SearchProduct`

--- ersatz/api.py
+++ ersatz/api.py
@@ -14,7 +14,26 @@ from .config import API, FIELD_KEPT
 
 def get_json(url, payload):
     """
-    Request API
+    A wrap over `Requests` & `json` library, to centralize API calls
+
+    Returns a dict() :
+     - `traceback` if an exception is raised
+     - `api_json` if the API calls gets valid response
+
+    The dict() is formated like this :
+
+    Unvalid :
+    {
+      'satus': False,
+      'context': method path,
+      'error': {details/message},
+    }
+
+    Valid :
+    {
+      'satus': True,
+      'products': { product dict() },
+    }
     """
     traceback = {'context': __name__+'.get_json() method','status': False}
 
@@ -48,8 +67,8 @@ class SearchProduct:
     """ Connects to API, get data, purge unused field and format some """
 
     def __init__(self, string):
-        self._url = API['PARAM_SEARCH']
-        self._payload = API['PARAM_SEARCH'].update({'search_terms': string})
+        self._payload = API['PARAM_SEARCH']
+        self._payload.update({'search_terms': string})
         self._products = []
         self._products_from_api = self._get_products_from_api()
 
@@ -77,7 +96,6 @@ class SearchProduct:
             # Parsing is done correctly, add check-fields
             self._products_from_api = {
                 'products': self._products,
-                'context': 'response',
                 'status': True,
             }
 
