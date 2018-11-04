7de54d54365bd95d77d976e7ddc1870b9fce44f1
Fred Z <freezed@users.noreply.github.com>
2018-11-04 13:28:03 +0100

    Add django loggin for developpement

--- omega/settings.py
+++ omega/settings.py
@@ -32,7 +32,39 @@ else:
         '0.0.0.0',
         '127.0.0.1',
         'ocp8-1664.herokuapp.com',
-    ]
+    ],
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'verbose': {
+                'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                           'pathname=%(pathname)s lineno=%(lineno)s ' +
+                           'funcname=%(funcName)s %(message)s'),
+                'datefmt': '%Y-%m-%d %H:%M:%S'
+            },
+            'simple': {
+                'format': '%(levelname)s %(message)s'
+            }
+        },
+        'handlers': {
+            'null': {
+                'level': 'DEBUG',
+                'class': 'logging.NullHandler',
+            },
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'verbose'
+            }
+        },
+        'loggers': {
+            'testlogger': {
+                'handlers': ['console'],
+                'level': 'INFO',
+            }
+        }
+    }
 
 # Application definition
 
