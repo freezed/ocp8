c6174ba045bcab678cd661e7863e763b8a37f238
Fred Z <freezed@users.noreply.github.com>
2018-11-20 14:40:11 +0100

    Add DB requesting #64

--- ersatz/tests/test_commands.py
+++ ersatz/tests/test_commands.py
@@ -1,14 +1,40 @@
 import pytest
-from ersatz.management.commands.prodsync import Command
+import json
 
-def test_get_db_products():
+from ersatz.management.commands.prodsync import Command
+from ersatz.models import Product
+
+@pytest.fixture
+def fill_20():
+    """ get 20 products to populate DB """
+
+    with open("ersatz/tests/samples/processed-fromage-page_1.json", "r") as json_file:
+        PRODUCTS = json.load(json_file)['products']
+
+    for prod in PRODUCTS:
+        del(prod['categories'])
+        prod['ingredients_text'] = prod['ingredients_text'][:255]
+
+        if not prod['nutrition_grades']:
+            prod['nutrition_grades'] = ''
+
+        if not prod['image_front_thumb_url']:
+            prod['image_front_thumb_url'] = ''
+
+        Product.objects.create(**prod)
+
+
+@pytest.mark.django_db
+def test_get_db_products(fill_20):
     """ test a DB request to get products  """
 
     cmd_instance = Command()
     prods = cmd_instance.dbproducts()
 
-    assert isinstance(prods, dict)
     assert prods[0]['code'] == '3073780258098'
+    assert prods[19]['code'] == '3222110023961'
+    assert prods[0]['nutrition_grades'] == 'd'
+    assert prods[19]['nutrition_grades'] == 'd'
 
 
 """ for a product request api """
